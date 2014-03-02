ba7dca9bd62faba9631fc837bef2521ab8c9a2f5
Justin Marple <Jmarple@umass.edu>
2014-03-02 18:07:59 -0500

    more cleaning

--- sensors.h
+++
@@ -1,9 +0,0 @@
-#ifndef SENSORS_H
-#define SENSORS_H
-
-//Function Prototypes
-void setupADC(void);
-uint16_t ReadADC(uint8_t ch);
-int getIRSensorValue(volatile uint8_t *port, uint8_t pin, int analogChannel);
-
-#endif
