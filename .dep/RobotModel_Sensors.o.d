2a38b2f358d0583e197adc54991d7356cc5e56a1
Aaron Lucia <engineal@outlook.com>
2014-03-13 20:10:45 -0400

    Finally, hopefully determineNextMove() will be operational

--- .dep/RobotModel_Sensors.o.d
+++
@@ -1,4 +0,0 @@
-RobotModel/RobotModel_Sensors.o: RobotModel/RobotModel_Sensors.c \
-  RobotModel/RobotModel_Sensors.h
-
-RobotModel/RobotModel_Sensors.h:
--- .dep/RobotModel_StepperMotor.o.d
+++
@@ -1,7 +0,0 @@
-RobotModel/RobotModel_StepperMotor.o:  \
- RobotModel/RobotModel_StepperMotor.c \
-  RobotModel/RobotModel_StepperMotor.h RobotModel/RobotModel.h
-
-RobotModel/RobotModel_StepperMotor.h:
-
-RobotModel/RobotModel.h:
