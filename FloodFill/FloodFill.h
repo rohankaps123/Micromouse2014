1829b41172b3d249bdc2935817414d2973aaca4f
Justin Marple <Jmarple@umass.edu>
2014-03-16 21:19:39 -0400

    Removing Extra Files for MouseManager

--- FloodFill/FloodFill.h
+++
@@ -1,6 +0,0 @@
-#ifndef FLOODFILL_H
-#define FLOODFILL_H
-
-void initializeMaze(long cell[][16]);
-void floodFill(long mazecells[16][16], char goal);
-#endif
--- FloodFill/FloodFill_Debug.h
+++
@@ -1,8 +0,0 @@
-#ifndef FLOODFILL_DEBUG_H
-#define	FLOODFILL_DEBUG_H
-
-void printMaze(long maze[16][16]);
-void printWalls(long maze[16][16]);
-void wallFallowingSolve(void);
-
-#endif
